4f493fade1b60d38c9bdc76540a51f8a4f31c30a
Kim JinSeon <kwlstjs00@gmail.com>
2023-12-29 00:31:58 +0900

    step5

--- step5/src/n5622.java
+++ step5/src/n5622.java
@@ -11,26 +11,52 @@ public class n5622 {
         char arr[] = br.readLine().toCharArray();
         int time = 0;
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 'A' || arr[i] == 'B' || arr[i] == 'C') {
-                time += 1;
-            } else if (arr[i] == 'D' || arr[i] == 'E' || arr[i] == 'F') {
-                time += 2;
-            } else if (arr[i] == 'G' || arr[i] == 'H' || arr[i] == 'I') {
-                time += 3;
-            } else if (arr[i] == 'J' || arr[i] == 'K' || arr[i] == 'L') {
-                time += 4;
-            } else if (arr[i] == 'M' || arr[i] == 'N' || arr[i] == 'O') {
-                time += 5;
-            } else if (arr[i] == 'P' || arr[i] == 'Q' || arr[i] == 'R' || arr[i] == 'S') {
-                time += 6;
-            } else if (arr[i] == 'T' || arr[i] == 'U' || arr[i] == 'V') {
-                time += 7;
-            } else if (arr[i] == 'W' || arr[i] == 'X' || arr[i] == 'Y' || arr[i] == 'Z') {
-                time += 8;
-            } else if (arr[i] == 'A' || arr[i] == 'B' || arr[i] == 'C') {
-                time += 9;
+            switch (arr[i]) {
+                case 'A':
+                case 'B':
+                case 'C':
+                    time += 3;
+                    break;
+                case 'D':
+                case 'E':
+                case 'F':
+                    time += 4;
+                    break;
+                case 'G':
+                case 'H':
+                case 'I':
+                    time += 5;
+                    break;
+                case 'J':
+                case 'K':
+                case 'L':
+                    time += 6;
+                    break;
+                case 'M':
+                case 'N':
+                case 'O':
+                    time += 7;
+                    break;
+                case 'P':
+                case 'Q':
+                case 'R':
+                case 'S':
+                    time += 8;
+                    break;
+                case 'T':
+                case 'U':
+                case 'V':
+                    time += 9;
+                    break;
+                case 'W':
+                case 'X':
+                case 'Y':
+                case 'Z':
+                    time += 10;
+                    break;
+                default:
+                    break;
             }
-            time += 2;
         }
         System.out.println(time);
     }
